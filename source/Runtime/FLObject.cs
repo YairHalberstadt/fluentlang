80f0e8d6b830d0eef732ceba4c441a401e02dc5d
yair halberstadt <yairhalberstadt@gmail.com>
2019-11-28 21:37:32 +0200

    Create initial minimilastic runtime. More will be added as is shown to be necessary by work on compiling down to it.

---
+++ source/Runtime/FLObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace FluentLang.Runtime
+{
+	public class FLObject
+	{
+		public static FLObject Empty { get; } = new FLObject();
+		private FLObject() => _methods = ImmutableDictionary<MethodKey, Delegate>.Empty;
+
+		private FLObject(ImmutableDictionary<MethodKey, Delegate> methods)
+		{
+			_methods = methods;
+		}
+
+		private readonly ImmutableDictionary<MethodKey, Delegate> _methods;
+
+		public T GetMethod<T>(MethodKey methodKey) where T : Delegate
+		{
+			if (!_methods.TryGetValue(methodKey, out var method))
+			{
+				throw new InvalidOperationException(
+$@"Method with MethodKey {methodKey.ToString()} does not exist on this.
+Make sure this is running with the same versions of dependencies as it was compiled against.");
+			}
+			if (!(method is T t))
+			{ 
+				throw new InvalidOperationException(
+$@"Method with MethodKey {methodKey.ToString()} is of type {method.GetType()} but was expected to be of type {typeof(T)}.
+Make sure this is running with the same versions of dependencies as it was compiled against.");
+			}
+			return t;
+		}
+
+		public FLObject With(MethodKey methodKey, Delegate method)
+		{
+			return new FLObject(_methods.SetItem(methodKey, method));
+		}
+	}
+}
---
+++ source/Runtime/MethodKey.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FluentLang.Runtime
+{
+	public struct MethodKey : IEquatable<MethodKey>
+	{
+		private readonly string _key;
+
+		public MethodKey(string key)
+		{
+			_key = key;
+		}
+
+		public bool Equals(MethodKey other)
+		{
+			return other._key?.Equals(_key) ??  
+				throw new InvalidOperationException($"{nameof(_key)} is null. Default MethodKeys are invalid.");
+		}
+
+		public override bool Equals(object? obj)
+		{
+			if (obj is MethodKey methodKey)
+				return Equals(methodKey);
+
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			return _key.GetHashCode();
+		}
+
+		public override string ToString()
+		{
+			return _key;
+		}
+	}
+}
