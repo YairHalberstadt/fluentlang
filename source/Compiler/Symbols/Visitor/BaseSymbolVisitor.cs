9c1d932be77a0e6005830cd1461979a0c19485fd
yair halberstadt <yairhalberstadt@gmail.com>
2020-02-27 18:35:34 +0200

    Reduce allocations in BaseSymbolVisitor. It may be possible to further reduce allocations by removing VisitMany.

--- source/Compiler/Symbols/Visitor/BaseSymbolVisitor.cs
+++ source/Compiler/Symbols/Visitor/BaseSymbolVisitor.cs
@@ -10,190 +10,162 @@ namespace FluentLang.Compiler.Symbols.Visitor
 	public abstract class BaseSymbolVisitor<T> : ISymbolVisitor<T>
 	{
 		[return: MaybeNull]
-		protected abstract T MergeResults(IEnumerable<T> results);
+		protected virtual T VisitMany(IEnumerable<IVisitableSymbol> symbols)
+		{
+			foreach(var symbol in symbols)
+			{
+				DefaultVisit(symbol);
+			}
+			return default;
+		}
 
 		[return: MaybeNull]
-		protected virtual T DefaultVisit<TSymbol>(TSymbol symbol, Func<TSymbol, IEnumerable<T>> visitChildren) where TSymbol : ISymbol
+		protected virtual T VisitMany(params IVisitableSymbol[] symbols)
+			=> VisitMany(symbols.AsEnumerable());
+
+		[return: MaybeNull]
+		protected virtual T DefaultVisit(IVisitableSymbol symbol)
 		{
-			return MergeResults(visitChildren(symbol));
+			return symbol.Visit(this);
 		}
 
 		[return: MaybeNull] 
 		public virtual T Visit(IMethod method)
 		{
-			return DefaultVisit(
-				method,
-				x => 
-					x.Parameters.Select(x => x.Visit(this))
-					.Append(x.ReturnType.Visit(this))
-					.Concat(x.Statements.Select(s => s.Visit(this))));
+			return VisitMany(
+					method.Parameters.AsEnumerable<IVisitableSymbol>()
+					.Append(method.ReturnType)
+					.Concat(method.Statements));
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IBinaryOperatorExpression binaryOperatorExpression)
 		{
-			return DefaultVisit(
-				binaryOperatorExpression,
-				x => new[] 
-				{ 
-					x.Left.Visit(this), 
-					x.Right.Visit(this) 
-				});
+			return VisitMany(binaryOperatorExpression.Left, binaryOperatorExpression.Right);
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IStaticInvocationExpression staticInvocationExpression)
 		{
-			return DefaultVisit(
-				staticInvocationExpression,
-				x => x.Arguments.Select(a => a.Visit(this)));
+			return VisitMany(
+				staticInvocationExpression.Arguments);
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IReturnStatement returnStatement)
 		{
-			return DefaultVisit(
-				returnStatement,
-				x => new[] { x.Expression.Visit(this) });
+			return DefaultVisit(returnStatement.Expression);
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IPrefixUnaryOperatorExpression prefixUnaryOperatorExpression)
 		{
-			return DefaultVisit(
-				prefixUnaryOperatorExpression,
-				x => new[] { x.Expression.Visit(this) });
+			return DefaultVisit(prefixUnaryOperatorExpression.Expression);
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(INewObjectExpression newObjectExpression)
 		{
-			return DefaultVisit(
-				newObjectExpression,
-				x => new T[0]);
+			return default;
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IObjectPatchingExpression objectPatchingExpression)
 		{
-			return DefaultVisit(
-				objectPatchingExpression,
-				x => x
-					.Patches
+			return VisitMany(
+				objectPatchingExpression.Patches
 					.Select(p => p.MixedInExpression)
 					.Where(m => m != null)
-					.Select(m => m!.Visit(this))
-					.Prepend(x.Expression.Visit(this)));
+					.Select(m => m!)
+					.Prepend(objectPatchingExpression.Expression));
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IMemberInvocationExpression memberInvocationExpression)
 		{
-			return DefaultVisit(
-				memberInvocationExpression,
-				x => x
-					.Arguments
-					.Select(a => a.Visit(this))
-					.Prepend(x.Expression.Visit(this)));
+			return VisitMany(
+				memberInvocationExpression.Arguments
+					.Select(a => a)
+					.Prepend(memberInvocationExpression.Expression));
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(ILocalReferenceExpression localReferenceExpression)
 		{
-			return DefaultVisit(
-				localReferenceExpression,
-				x => new T[0]);
+			return default;
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(ILiteralExpression literalExpression)
 		{
-			return DefaultVisit(
-				literalExpression,
-				x => new T[0]);
+			return default;
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IDeclarationStatement declarationStatement)
 		{
-			return DefaultVisit(
-				declarationStatement,
-				x => new[] { x.Expression.Visit(this) });
+			return DefaultVisit(declarationStatement.Expression);
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IConditionalExpression conditionalExpression)
 		{
-			return DefaultVisit(
-				conditionalExpression,
-				x => new[] { 
-					x.Condition.Visit(this),
-					x.IfTrue.Visit(this),
-					x.IfFalse.Visit(this)});
+			return VisitMany(
+					conditionalExpression.Condition,
+					conditionalExpression.IfTrue,
+					conditionalExpression.IfFalse);
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IMatchExpression matchExpression)
 		{
-			return DefaultVisit(
-				matchExpression,
-				x =>
-					x.Arms.Select(a => a.Visit(this))
-					.Prepend(x.Expression.Visit(this)));
+			return VisitMany(
+					matchExpression.Arms.AsEnumerable<IVisitableSymbol>()
+					.Prepend(matchExpression.Expression));
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IMatchExpressionArm matchExpressionArm)
 		{
-			return DefaultVisit(
-				matchExpressionArm,
-				x => new[] { x.Expression.Visit(this) });
+			return DefaultVisit(matchExpressionArm.Expression);
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(Primitive primitive)
 		{
-			return DefaultVisit(
-				primitive,
-				x => new T[0]);
+			return default;
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IUnion union)
 		{
-			return DefaultVisit(
-				union,
-				x => x.Options.Select(x => x.Visit(this)));
+			return VisitMany(union.Options);
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IInterface @interface)
 		{
-			return DefaultVisit(
-				@interface,
-				x => x.Methods.Select(x => x.Visit(this)));
+			return VisitMany(@interface.Methods);
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(IInterfaceMethod method)
 		{
-			return DefaultVisit(
-				method,
-				x => x.Parameters.Select(x => x.Visit(this)).Append(x.ReturnType.Visit(this)));
+			return VisitMany(
+				method.Parameters.AsEnumerable<IVisitableSymbol>()
+				.Append(method.ReturnType));
 		}
 
 		[return:MaybeNull]
 		public virtual T Visit(IParameter parameter)
 		{
-			return DefaultVisit(
-				parameter,
-				x => new[] { x.Type.Visit(this) });
+			return DefaultVisit(parameter.Type);
 		}
 
 		[return: MaybeNull]
 		public virtual T Visit(ITypeParameter typeParameter)
 		{
-			return DefaultVisit(typeParameter, x => new T[0]);
+			return default;
 		}
 	}
 }
--- source/Compiler/Symbols/Visitor/SymbolExtensions.cs
+++ source/Compiler/Symbols/Visitor/SymbolExtensions.cs
@@ -1,24 +1,47 @@
 ﻿using FluentLang.Compiler.Symbols.Interfaces;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics.CodeAnalysis;
 
 namespace FluentLang.Compiler.Symbols.Visitor
 {
 	public static class SymbolExtensions
 	{
-		public static IEnumerable<ISymbol> DescendantNodesAndSelf(this IVisitableSymbol symbol)
+		public static IEnumerable<ISymbol> DescendantNodes(this IVisitableSymbol symbol, Func<ISymbol, bool>? visitChildren = null, Func<ISymbol, bool>? includeSymbol = null)
 		{
-			return symbol.Visit(CollectAllSymbolsVisitor.DefaultInstance);
+			var visitor = new CollectAllSymbolsVisitor(
+				visitChildren ?? (_ => true),
+				includeSymbol ?? (_ => true));
+			symbol.Visit(visitor);
+			return visitor.Symbols;
 		}
 
-		public static IEnumerable<ISymbol> DescendantNodes(this IVisitableSymbol symbol)
+		private sealed class CollectAllSymbolsVisitor : BaseSymbolVisitor<object>
 		{
-			return symbol.Visit(CollectAllSymbolsVisitor.DefaultInstance).Skip(1);
-		}
+			public readonly List<ISymbol> Symbols = new List<ISymbol>();
 
-		public static bool IsDescendantOrThis(this IVisitableSymbol symbol, ISymbol target)
-		{
-			return symbol.Visit(new IsDescendantVisitor(target));
+			private readonly Func<ISymbol, bool> _visitChildren;
+			private readonly Func<ISymbol, bool> _includeSymbol;
+
+			public CollectAllSymbolsVisitor(Func<ISymbol, bool> visitChildren, Func<ISymbol, bool> includeSymbol)
+			{
+				_visitChildren = visitChildren;
+				_includeSymbol = includeSymbol;
+			}
+
+			[return: MaybeNull]
+			protected override object DefaultVisit(IVisitableSymbol symbol)
+			{
+				if (_includeSymbol(symbol))
+				{
+					Symbols.Add(symbol);
+				}
+				if (_visitChildren(symbol))
+				{
+					return symbol.Visit(this);
+				}
+				return default;
+			}
 		}
 	}
 }
