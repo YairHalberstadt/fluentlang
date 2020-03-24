ebd37cfd16b7ba43d4cf100ea13043d757519437
yair halberstadt <yairhalberstadt@gmail.com>
2020-03-24 07:50:20 +0200

    Add extra test for Recursive generic test with different type arguments

--- source/Compiler.Tests.Unit/Symbols/SourceSymbols/MethodBodyTests/RecursiveGenericCallTests.cs
+++ source/Compiler.Tests.Unit/Symbols/SourceSymbols/MethodBodyTests/RecursiveGenericCallTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentLang.TestUtils;
+﻿using FluentLang.Compiler.Diagnostics;
+using FluentLang.TestUtils;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -72,7 +73,7 @@ export M2<T>(a: int) : int {
 		}
 
 		[Fact]
-		public void WithDifferentArgumentsWithRequiredMethodKeys()
+		public void WithDifferentArgumentsWithoutRequiredMethodKeys1()
 		{
 			CreateAssembly(@"
 export Main() : int { 
@@ -102,6 +103,43 @@ export M2<T>(a: int, b: T) : { GetT() : T; } {
 }")
 	.VerifyDiagnostics()
 	.VerifyEmit(expectedResult: 42);
+		}
+
+		[Fact]
+		public void WithDifferentArgumentsWithRequiredMethodKeys()
+		{
+			CreateAssembly(@"
+export Main() : int { 
+	return M<int>(5, 42).GetT();
+}
+
+export M<T>(a : int, b: T) : { GetT() : T; } {
+	return M1<T>(a, b);
+}
+
+export M1<T>(a : int, b: T) : { GetT() : T; } {
+	return M2<T>(a, b);
+}
+
+export M2<T>(a: int, b: T) : { GetT() : T; } {
+	return M3<T>(b);
+	M3<T1>(c : T1) : { GetT() : T1; } {
+		return M4();
+		M4() : { GetT() : T1; } {
+			let x = {} + GetT;
+			_ = if (a == 0) {} else M<{ GetT() : T1; }>(a - 1, x);
+			return x;
+			GetT(d : {}) : T1 {
+				return c;
+			}
+		}
+	}
+}")
+	.VerifyDiagnostics(
+					new Diagnostic(new Location(new TextToken(@"exportM<T>(a:int,b:T):{GetT():T;}{returnM1<T>(a,b);}")), ErrorCode.RecursiveFunctionCallWithDifferentTypeArguments),
+					new Diagnostic(new Location(new TextToken(@"exportM1<T>(a:int,b:T):{GetT():T;}{returnM2<T>(a,b);}")), ErrorCode.RecursiveFunctionCallWithDifferentTypeArguments),
+					new Diagnostic(new Location(new TextToken(@"exportM2<T>(a:int,b:T):{GetT():T;}{returnM3<T>(b);M3<T1>(c:T1):{GetT():T1;}{returnM4();M4():{GetT():T1;}{letx={}+GetT;_=if(a==0){}elseM<{GetT():T1;}>(a-1,x);returnx;GetT(d:{}):T1{returnc;}}}}")), ErrorCode.RecursiveFunctionCallWithDifferentTypeArguments),
+					new Diagnostic(new Location(new TextToken(@"M3<T1>(c:T1):{GetT():T1;}{returnM4();M4():{GetT():T1;}{letx={}+GetT;_=if(a==0){}elseM<{GetT():T1;}>(a-1,x);returnx;GetT(d:{}):T1{returnc;}}}")), ErrorCode.RecursiveFunctionCallWithDifferentTypeArguments));
 		}
 	}
 }
