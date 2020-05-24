2fb47742dd700e270811579e9c7d05a91597e716
yair halberstadt <yairhalberstadt@gmail.com>
2020-05-24 17:36:59 +0300

    Make generate-parser calling directory independent

--- build-script/generate-parser.ps1
+++ build-script/generate-parser.ps1
@@ -1,11 +1,11 @@
 Write-Output "Generating FluentLangParser"
 
-java -jar ./antlr-4.7.2-complete.jar `
-    -lib ../grammar `
-    -o ../source/Compiler/Generated `
+java -jar (Join-Path -Path $PSScriptRoot -ChildPath antlr-4.7.2-complete.jar) `
+    -lib (Join-Path -Path $PSScriptRoot -ChildPath ../grammar) `
+    -o (Join-Path -Path $PSScriptRoot -ChildPath ../source/Compiler/Generated) `
     -listener `
     -visitor `
     -Dlanguage=CSharp `
     -package FluentLang.Compiler.Generated `
-    ../grammar/FluentLangLexer.g4 `
-    ../grammar/FluentLangParser.g4
+    (Join-Path -Path $PSScriptRoot -ChildPath ../grammar/FluentLangLexer.g4) `
+    (Join-Path -Path $PSScriptRoot -ChildPath ../grammar/FluentLangParser.g4)
